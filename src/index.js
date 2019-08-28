748afc6e22ddf93992681c61965ccec24d9298a6
Russel Povey <russel@bigbrave.co.za>
2019-08-28 16:22:31 +0200

    proof of concept

--- src/index.js
+++ src/index.js
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useState, useEffect, useContext, createContext } from "react";
 
-const ReactColorSquare = props => {
-  const { width, height, color, text } = props;
+const DataStoreContext = createContext();
+
+const DataStore = props => {
+  const { firebase, collections, children } = props;
+  const [state, setState] = React.useState({});
+
+  React.useEffect(() => {
+    const listeners = collections.map(setCollectionListener);
+    return () => listeners.forEach(listener => listener());
+  }, [collections, firebase]);
+
+  const setCollectionListener = collection =>
+    firebase
+      .firestore()
+      .collection(collection)
+      .onSnapshot(handleSnapshot);
+
+  const handleSnapshot = snapshot => {
+    const collection = snapshot.query._query.path.segments[0];
+    const formatedDocs = snapshot.docs.map(formatDocSnap);
+    setState(p => ({ ...p, [collection]: formatedDocs }));
+  };
+
+  const formatDocSnap = docSnap => ({ id: docSnap.id, ...docSnap.data() });
+
+  console.log(state);
+
   return (
-    <div
-      style={{
-        width: width || 100,
-        height: height || 100,
-        backgroundColor: color || "blue"
-      }}
-    >
-      {text}
-    </div>
+    <DataStoreContext.Provider value={state}>
+      {children}
+    </DataStoreContext.Provider>
   );
 };
 
-export default ReactColorSquare;
+function useDataStore() {
+  return useContext(DataStoreContext);
+}
+
+export default DataStore;
+export { useDataStore };
